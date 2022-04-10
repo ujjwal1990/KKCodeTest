c2b9cc02ace309ee799221497dde0466ef9b4db1
Ujjwal Bansal <ujjwal.bansal03@gmail.com>
2022-04-10 18:23:26 +0100

    updated activity to support custom toolbar

--- app/src/main/java/com/kk/codetest/ui/MainActivity.kt
+++ app/src/main/java/com/kk/codetest/ui/MainActivity.kt
@@ -1,29 +1,28 @@
 package com.kk.codetest.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.kk.codetest.R
-import com.kk.codetest.ui.menu.MenuFragment
-import com.kk.codetest.ui.welcome.WelcomeFragment
+import com.kk.codetest.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val defaultHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        val navController = (defaultHostFragment as NavHostFragment).navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         return NavigationUI.navigateUp(navController, AppBarConfiguration(navController.graph))
     }
 }
