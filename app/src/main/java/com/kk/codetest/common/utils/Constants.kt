d7b2dec4ff03543a01b93c5c4eb9b21db164cdf2
Ujjwal Bansal <ujjwal.bansal03@gmail.com>
2022-04-12 13:41:24 +0100

    updated base url in constants

--- app/src/main/java/com/kk/codetest/common/utils/Constants.kt
+++ app/src/main/java/com/kk/codetest/common/utils/Constants.kt
@@ -2,6 +2,8 @@ package com.kk.codetest.common.utils
 
 class Constants {
     companion object {
+        const val BASE_URL =
+            "https://eu-west-1.aws.data.mongodb-api.com/app/chatreward-kytrn/endpoint/"
         const val EMPTY = ""
     }
 }
